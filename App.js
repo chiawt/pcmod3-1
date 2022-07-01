9ed93fc55bbf40ae7110ce02f1375d8f39fda3d9
WeeTiong Chia <xiewz78@hotmail.com>
2022-07-01 22:20:23 +0800

    Add button for addition random color

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList,TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <BlockRGB red={255} green={0} blue={0} />
-      <BlockRGB red={0} green={255} blue={0} />
-      <BlockRGB red={0} green={0} blue={255} />
-    </View>
-  );
+ const [colorArray, setColorArray] = useState([
+   { red: 255, green: 0, blue: 0, id: "0" },
+   { red: 0, green: 255, blue: 0, id: "1" },
+   { red: 0, green: 0, blue: 255, id: "2" },
+ ]);
+
+ function AddColor() {
+  setColorArray([
+    ...colorArray,
+    {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: `${colorArray.length}`,
+    },
+  ]);
 }
- 
+
+return (
+  <View style={styles.container}>
+    <TouchableOpacity
+      style={{ height: 40, justifyContent: "center" }}
+      onPress={AddColor}
+    >
+      <Text style={{ color: "red" }}>Add colour</Text>
+    </TouchableOpacity>
+    <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+  </View>
+);
+
+
+ function renderItem({ item }) {
+   return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+ }
+}
 
 const Stack = createStackNavigator();
 
@@ -22,7 +48,7 @@ export default function App() {
  return (
    <NavigationContainer>
      <Stack.Navigator>
-       <Stack.Screen name="Home" component={HomeScreen} />
+       <Stack.Screen name="Colour List" component={HomeScreen} />
      </Stack.Navigator>
    </NavigationContainer>
  );
@@ -33,8 +59,8 @@ const styles = StyleSheet.create({
    flex: 1,
    backgroundColor: "#fff",
    alignItems: "center",
-   justifyContent: "center",
+ },
+ list: {
+   width: "30%",
  },
 });
-
-
