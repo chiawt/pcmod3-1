158647c14287e4549f65f7744c355fbf0f4b49ff
WeeTiong Chia <xiewz78@hotmail.com>
2022-07-02 10:45:00 +0800

    Added navigation

--- App.js
+++ App.js
@@ -5,20 +5,29 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
  const [colorArray, setColorArray] = useState([0]);
 
- function addColor() {
-  setColorArray([
-    ...colorArray,
-    {
-      red: Math.floor(Math.random() * 256),
-      green: Math.floor(Math.random() * 256),
-      blue: Math.floor(Math.random() * 256),
-      id: colorArray.length.toString(),
-    },
-  ]);
-}
+
+  function renderItem({ item }) {
+    return (
+      <TouchableOpacity onPress={() => navigation.navigate("DetailsScreen")}>
+        <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+      </TouchableOpacity>
+    );
+  }
+
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: colorArray.length.toString(),
+      },
+    ]);
+  }
 
 function resetColor() {
   setColorArray([]);
@@ -43,10 +52,10 @@ return (
     <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
   </View>
 );
+}
 
- function renderItem({ item }) {
-   return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
- }
+function DetailsScreen(){
+  return <Text>DetailsScreen</Text>
 }
 
 const Stack = createStackNavigator();
@@ -56,6 +65,7 @@ export default function App() {
    <NavigationContainer>
      <Stack.Navigator>
        <Stack.Screen name="Colour List" component={HomeScreen} />
+       <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
      </Stack.Navigator>
    </NavigationContainer>
  );
